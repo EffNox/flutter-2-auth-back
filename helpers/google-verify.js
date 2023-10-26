3f5804c423083319bed561a3d8dd06fcdf9447e8
ℱєгภคภ๔๏ <52844988+EffNox@users.noreply.github.com>
2023-10-26 13:22:18 -0500

    DOT

--- helpers/google-verify.js
+++ helpers/google-verify.js
@@ -1,7 +1,7 @@
 const { OAuth2Client } = require('google-auth-library');
 
-const CLIENT_ID = '336279698109-mq43ct96j81r5soefagred3044rdu563.apps.googleusercontent.com';
-const CLIENT_ANDROID = '336279698109-veo2qmnfujl3udf8s8ppqhed3cc9m2se.apps.googleusercontent.com';
+const CLIENT_ID = 'X';
+const CLIENT_ANDROID = 'X';
 
 const client = new OAuth2Client(CLIENT_ID);
 const validateGGToken = async (token) => {
